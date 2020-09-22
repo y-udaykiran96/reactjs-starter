62d014fd6d14d6a3c33ac1928d15165eb469b84e
y-udaykiran96 <y.udaykiran96@gmail.com>
2020-09-22 12:40:18 +0530

    added vidly as sub project

--- src/vidly/App.js
+++ src/vidly/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './app.css';
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Vidly project
         </p>
         <a
           className="App-link"
